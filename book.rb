c8ea0cd49747dee4a428055215f1c3fa988f3b75
Chris Philbin <chris@chrisphilbin.net>
2019-01-25 20:49:56 -0500

    rubocop

--- book.rb
+++ book.rb
@@ -1,24 +1,22 @@
 class Book
+  attr_accessor :book_title, :book_author, :book_published
 
-	attr_accessor :book_title, :book_author, :book_published
+  @@all = []
 
-	@@all = []
+  def initialize(book_hash)
+    book_hash.each do |key, value|
+      send("#{key}=", value)
+      @@all << self
+    end
+   end
 
-  	def initialize(book_hash)
-    	book_hash.each do |key, value|
-      		self.send("#{key}=", value)
-      		@@all << self
-    	end
-  	end
+  def self.create_from_list(books)
+    books.each do |book_hash|
+      Book.new(book_hash)
+    end
+   end
 
-  	def self.create_from_list(books)
-    		books.each do |book_hash|
-      		Book.new(book_hash)
-    	end
-	end
-
-	def self.all
-		@@all
-	end
-
+  def self.all
+    @@all
+  end
 end
--- scraper.rb
+++ scraper.rb
@@ -1,24 +1,22 @@
-	require 'nokogiri'
-	require 'open-uri'
-	require 'pry'
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
 
-#"https://www.goodreads.com/shelf/show/100-books-to-read-before-you-die"
+# "https://www.goodreads.com/shelf/show/100-books-to-read-before-you-die"
 
 class Scraper
+  def self.scrape_page(page_url)
+    students = []
 
- 	def self.scrape_page(page_url)
-
-		students = []
-
-		page = Nokogiri::HTML(open(page_url))
-		page.css("div.leftContainer").each do |container|
-			container.css("div.elementList").each do |element|
-				title = element.css("a.leftAlignedImage").attr('title').text
-				author = element.css("div.authorName__container a").text
-				published = element.css("span.greyText").text.gsub(/\s+/, "")[-4..-1]
-				students << { :book_title => title, :book_author => author, :book_published => published}
-			end
-		end
-		students
-	end	
+    page = Nokogiri::HTML(open(page_url))
+    page.css('div.leftContainer').each do |container|
+      container.css('div.elementList').each do |element|
+        title = element.css('a.leftAlignedImage').attr('title').text
+        author = element.css('div.authorName__container a').text
+        published = element.css('span.greyText').text.gsub(/\s+/, '')[-4..-1]
+        students << { book_title: title, book_author: author, book_published: published }
+      end
+    end
+    students
+  end
 end
