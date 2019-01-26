ceafd0ef96b52e882aa71847a32f483ddadacc83
Chris Philbin <chris@chrisphilbin.net>
2019-01-25 19:48:34 -0500

    Working on scraping features

--- scraper.rb
+++ scraper.rb
@@ -2,13 +2,15 @@
 	require 'open-uri'
 	require 'pry'
 
-# class Scraper
+#"https://www.goodreads.com/shelf/show/100-books-to-read-before-you-die"
 
-# 	def scrape_page
+class Scraper
+
+ 	def self.scrape_page(page_url)
 
 		students = []
 
-		page = Nokogiri::HTML(open("https://www.goodreads.com/shelf/show/100-books-to-read-before-you-die"))
+		page = Nokogiri::HTML(open(page_url))
 		page.css("div.leftContainer").each do |container|
 			container.css("div.elementList").each do |element|
 				title = element.css("a.leftAlignedImage").attr('title').text
@@ -17,18 +19,6 @@
 				students << { :book_title => title, :book_author => author, :book_published => published}
 			end
 		end
-
-		binding.pry
-
-
-		# page.css("div.leftContainer").each do |container|
-		# 	container.css("elementList").each do |element|
-		# 		book_title = element.css("div.left ")
-		# 		book_author = element.css("div.left div.author_Name__container").text
-		# 		book_published =
-		# 	end
-		# end
-
-# 	end
-
-# end
+		students
+	end	
+end
