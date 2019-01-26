ae1b32c71b65c63fa5500117cabba4cd6d789f7f
Chris Philbin <chris@chrisphilbin.net>
2019-01-26 08:50:00 -0500

    Working on CLI logic

--- lib/command-line-interface.rb
+++ lib/command-line-interface.rb
@@ -19,16 +19,27 @@ class CommandLineInterface
 		end
 	end
 
-end
 
-user_input = ""
+	def call
+		user_input = ""
 
-while user_input != "exit"
-	puts "Welcome to the best books gem! This gem will scrape the website Good Reads and return a current list of their 100 books to read before you die!"
-	puts "Please enter a selection based on the below:"
-	puts "Enter 'list' to get the most current list of books, in order from best to worst."
-	puts "Enter 'random' to get the list, but in random order."
-	puts 'Enter 'exit' to exit this program."
+		while user_input != "exit"
+			puts "Welcome to the best books gem! This gem will scrape the website Good Reads and return a current list of their 100 books to read before you die!"
+			puts "Please enter a selection based on the below:"
+			puts "Enter 'list' to get the most current list of books, in order from best to worst."
+			puts "Enter 'random' to get the list, but in random order."
+			puts "Enter 'exit' to exit this program."
+
+			user_input = gets.chomp
+
+			case user_input
+			when "list"
+				make_book_list
+			when "random"
+				make_random_list
+			end
+	end
+
 end
 
 binding.pry
