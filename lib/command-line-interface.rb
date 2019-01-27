5abd67a3bcb6408129ff02f267b044a9ae73f62c
Chris Philbin <chris@chrisphilbin.net>
2019-01-26 22:11:56 -0500

    Continuing to work on CLI logic

--- lib/command-line-interface.rb
+++ lib/command-line-interface.rb
@@ -1,5 +1,5 @@
-require_relative "../ruby-cli-project/scraper.rb"
-require_relative "../ruby-cli-project/book.rb"
+require_relative "../lib/scraper.rb"
+require_relative "../lib/book.rb"
 
 require 'nokogiri'
 require 'pry'
@@ -8,22 +8,23 @@ class CommandLineInterface
 
 	page_url = "https://www.goodreads.com/shelf/show/100-books-to-read-before-you-die"
 
-	def make_book_list(page_url)
+	def self.make_book_list(page_url)
 		book_array = Scraper.scrape_page(page_url)
 		Book.create_from_list(book_array)
 	end
 
-	def set_book_age
+	def self.set_book_age
 		Book.all.each do |book|
 			book.book_age = Time.now.year - self.book_published.to_i
 		end
 	end
 
-	def random
+	def self.make_random_list
 		random_books = Book.all.shuffle
-		random_books.each_with_index(1) do |book, index|
-			puts "#{index}. #{book.book_title} by #{book.book_author}"
+		random_books.each_with_index.map do |book, index|
+			puts "#{index}. #{book.book_title} by #{book.book_author}, published in #{book.book_published}"
 		end
+		random_books
 	end
 
 
@@ -45,8 +46,7 @@ class CommandLineInterface
 			when "random"
 				make_random_list
 			end
+		end
 	end
-
-end
-
 binding.pry
+end
